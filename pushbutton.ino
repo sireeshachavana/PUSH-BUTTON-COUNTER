60beb119560b3b33c76919e977e24f958afbe5e9
sireeshachavana <reddysireeshachavana@gmail.com>
2025-06-20 11:59:01 +0530

    Create pushbutton.ino

---
+++ pushbutton.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal.h>
+int button=8;
+int val;
+int count=0;
+int press;
+const int rs = 12,en = 11,d4 = 5, d5 = 4, d6 = 3,d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+void setup()
+{
+  lcd.begin(16, 2);
+  pinMode(button, INPUT);
+}
+
+void loop()
+{
+  val=digitalRead(button);
+  if(val=HIGH){
+    press=count++;
+    delay(250);
+  }
+  lcd.setCursor(0, 0);
+  lcd.print("Button Count");
+  lcd.setCursor(0, 1);
+  lcd.print(press);
+  
+}
